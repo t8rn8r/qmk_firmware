8b17fc36d1ec33fcca38455ead6ed32d2ac35e70
t8rn8r <wintermydog@gmail.com>
2021-01-06 11:10:24 -0600

    formatting changes before improving current layers

--- keyboards/crkbd/keymaps/t8rn8r/keymap.c
+++ keyboards/crkbd/keymaps/t8rn8r/keymap.c
@@ -25,10 +25,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,			KC_Y, KC_U, KC_I,    KC_O,   KC_P,    XXXXXXX,
-       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,			KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        MO(1),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,			KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(2),  
-                                          KC_LGUI, TD(LS_TG1), BS_CTL,	SP_NAV, TD(RS_TG2), KC_RALT
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,			    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        MO(1),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,			    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   MO(2),  
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        KC_LGUI, TD(LS_TG1), BS_CTL,     SP_NAV, TD(RS_TG2), KC_RALT
   ),
 
   [1] = LAYOUT_split_3x6_3(
